f4ba8050fcae19dc54f8dc7dc1e97bb192f4b2e9
Mohammad Raza <mohammadraza714@gmail.com>
2024-11-14 00:45:00 +0500

    getCheckedItems helper updated. Continence and Level of Independence included in impression plan.

--- libs/helpers.js
+++ libs/helpers.js
@@ -266,7 +266,7 @@ export function getCheckedItems(arr, valueProp) {
     .filter((item) => item.IsChecked === "true")
     .map((item) => item[valueProp] || "");
 
-  return checkedItems;
+  return checkedItems.map(item => " " + item).join(",");
 }
 
 export function getDiagnosisAndICD(arr) {
